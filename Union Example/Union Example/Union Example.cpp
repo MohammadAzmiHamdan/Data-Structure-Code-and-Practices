2ddad09fc1961e867b70990ea00c110d7d160ca1
Mohammad Hmadan <mohammadhamdanh12@gmail.com>
2026-09-20 23:42:24 +0300

    Add files via upload

---
+++ Union Example/Union Example/Union Example.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+union MyUnion {
+    int intValue;
+    float floatValue;
+    char charValue;
+};
+using namespace std; 
+int main() {
+    MyUnion myUnion;
+
+    myUnion.intValue = 42;
+    std::cout << "Integer value: " << myUnion.intValue << std::endl;
+
+    myUnion.floatValue = 3.14f;
+    std::cout << "Float value: " << myUnion.floatValue << std::endl;
+
+    myUnion.charValue = 'A';
+    std::cout << "Char value: " << myUnion.charValue << std::endl;
+    return 0;
+}
